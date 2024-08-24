10de30c58be8f74a170f7e0004e9e6e925ee30c6
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-08-24 20:35:20 +0200

    Added zeek to gateway

--- docker/prototype_compose/gateway/entrypoint.sh
+++ docker/prototype_compose/gateway/entrypoint.sh
@@ -10,5 +10,8 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables -A FORWARD -i eth0 -o eth1 -j ACCEPT
+
+zeek -i eth0
+
 # Keep the container running (optional)
 tail -f /dev/null
