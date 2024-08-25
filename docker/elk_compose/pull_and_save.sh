5fb87c6bb9179f5e31ffcd461e5acfa63311c946
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-08-25 17:31:34 +0200

    Updated script

--- docker/elk_compose/pull_and_save.sh
+++ docker/elk_compose/pull_and_save.sh
@@ -4,9 +4,9 @@
 ELASTIC_VERSION=8.13.1
 
 TARGET=../../custom_elements/elk/files
-ELASTICSEARCH=docker.elastic.co/elasticsearch/elasticsearch:${ELASTIC_VERSION}
-KIBANA=docker.elastic.co/kibana/kibana:${ELASTIC_VERSION}
-LOGSTASH=docker.elastic.co/logstash/logstash:${ELASTIC_VERSION}
+ELASTICSEARCH="docker.elastic.co/elasticsearch/elasticsearch:$ELASTIC_VERSION"
+KIBANA="docker.elastic.co/kibana/kibana:$ELASTIC_VERSION"
+LOGSTASH="docker.elastic.co/logstash/logstash:$ELASTIC_VERSION"
 
 # Login
 docker login
