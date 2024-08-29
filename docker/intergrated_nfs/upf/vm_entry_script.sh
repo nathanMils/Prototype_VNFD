bbcb2e32f10f922e1ee37be602189680588a1ead
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-08-29 17:05:03 +0200

    Moved upf

--- docker/intergrated_nfs/upf/vm_entry_script.sh
+++ docker/intergrated_nfs/upf/vm_entry_script.sh
@@ -9,20 +9,22 @@ EXTERNAL_INTERFACE="ens3"           # Interface assigned to the external network
 # DNS Name resolution
 ELK_IP=$2
 NRF_IP=$3
+MYSQL_IP=$4
 
 # Update hosts for DNS Name resolution
 echo "$ELK_IP elasticsearch" >> /etc/hosts
 echo "$ELK_IP logstash" >> /etc/hosts
 echo "$NRF_IP oai-nrf" >> /etc/hosts
+echo "$MYSQL_IP mysql" >> /etc/hosts
 
-ELK_GATEWAY=$3
+ELK_GATEWAY=$5
 
 # Add routing via elk network to not disturb NF
 sudo ip route add $ELK_IP via $ELK_GATEWAY
 
 # Exporting required vars
-export FILEBEAT_INTERNAL_PASSWORD=$4
-export BEATS_SYSTEM_PASSWORD=$5
+export FILEBEAT_INTERNAL_PASSWORD=$6
+export BEATS_SYSTEM_PASSWORD=$7
 
 # Starting containers and networks
 TARGET_DIR=/opt/upf
