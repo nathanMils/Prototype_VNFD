e3791c937cd5340fe1a0385e52d59db73562fb99
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-08-30 17:08:37 +0200

    SMF V2

--- docker/intergrated_nfs/nrf/vm_entry_script.sh
+++ docker/intergrated_nfs/nrf/vm_entry_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # DNS Name resolution
-ELK_IP=$2
-NRF_IP=$3
-MYSQL_IP=$4
+ELK_IP=$1
+NRF_IP=$2
+MYSQL_IP=$3
 
 # Update hosts for DNS Name resolution
 echo "$ELK_IP elasticsearch" >> /etc/hosts
@@ -11,17 +11,17 @@ echo "$ELK_IP logstash" >> /etc/hosts
 echo "$NRF_IP oai-nrf" >> /etc/hosts
 echo "$MYSQL_IP mysql" >> /etc/hosts
 
-ELK_GATEWAY=$5
+ELK_GATEWAY=$4
 
 # Add routing via elk network to not disturb NF
 sudo ip route add $ELK_IP via $ELK_GATEWAY
 
 # Exporting required vars
-export FILEBEAT_INTERNAL_PASSWORD=$6
-export BEATS_SYSTEM_PASSWORD=$7
+export FILEBEAT_INTERNAL_PASSWORD=$5
+export BEATS_SYSTEM_PASSWORD=$6
 export INTERFACE="ens3"
 
 # Starting containers and networks
-TARGET_DIR=/opt/nrf
-docker load -i $TARGET_DIR/nrf-images.tar
-docker compose -f $TARGET_DIR/docker-compose-nrf-built.yml up -d
+TARGET_DIR=/opt/smf
+docker load -i $TARGET_DIR/smf-images.tar
+docker compose -f $TARGET_DIR/docker-compose-smf-built.yml up -d
--- docker/intergrated_nfs/smf/oai-smf/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ip route del default via 172.20.0.1
-ip route add default via 172.20.0.2
-ip route show
-# Execute original entrypoint
-exec "$@"
