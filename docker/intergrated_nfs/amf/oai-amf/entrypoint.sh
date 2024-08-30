765378bb97a1e907022d63cfa137f4fade3b0701
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-08-30 16:28:53 +0200

    Enp0s3

--- docker/intergrated_nfs/amf/oai-amf/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Perform substitution and write to a temporary file
-envsubst < /openair-amf/etc/config.yaml > /openair-amf/etc/config.tmp.yaml
-# Replace the original file with the temporary file
-mv /openair-amf/etc/config.tmp.yaml /openair-amf/etc/config.yaml
-
-# Start the main application
-exec "$@"
