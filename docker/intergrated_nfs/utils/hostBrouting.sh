2cb9df0f23c582910f1ca7fde19d8fd6cd8b1d3b
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-09-02 12:48:21 +0200

    Update

--- docker/intergrated_nfs/utils/hostBrouting.sh
+++ docker/intergrated_nfs/utils/hostBrouting.sh
@@ -7,15 +7,15 @@ ELK_NET_A_CIDR_V4="10.6.0.0/26"
 HOST_A_IP_V4="fill in"                     # IPv4 address of Host A
 
 # HOST A IPv6 info
-5G_NET_A_CIDR_V6="2001:db8:abcd:001::/64"
-ELK_NET_A_CIDR_V6="2001:db8:abcd:002::/64"
-HOST_A_IP_V6="fill in"                     # IPv6 address of Host A
+# 5G_NET_A_CIDR_V6="2001:db8:abcd:001::/64"
+# ELK_NET_A_CIDR_V6="2001:db8:abcd:002::/64"
+# HOST_A_IP_V6="fill in"                     # IPv6 address of Host A
 
 # Routing for Host A
 ip route add $5G_NET_A_CIDR_V4 via $HOST_A_IP_V4
 ip route add $ELK_NET_A_CIDR_V4 via $HOST_A_IP_V4
-ip -6 route add $5G_NET_A_CIDR_V6 via $HOST_A_IP_V6
-ip -6 route add $ELK_NET_A_CIDR_V6 via $HOST_A_IP_V6
+# ip -6 route add $5G_NET_A_CIDR_V6 via $HOST_A_IP_V6
+# ip -6 route add $ELK_NET_A_CIDR_V6 via $HOST_A_IP_V6
 
 # HOST B IPv4 info
 5G_NET_B_CIDR_V4="10.7.0.0/26"
@@ -24,13 +24,13 @@ VIR_ROUTER_B_EXT_IP_V4="fill in"           # IPv4 address of the external router
 HOST_B_IP_V4="fill in"                     # IPv4 address of Host B
 
 # HOST B IPv6 info
-5G_NET_B_CIDR_V6="2001:db8:abcd:003::/64"
-ELK_NET_B_CIDR_V6="2001:db8:abcd:004::/64"
-VIR_ROUTER_B_EXT_IP_V6="fill in"           # IPv6 address of the external router
-HOST_B_IP_V6="fill in"                     # IPv6 address of Host B
+# 5G_NET_B_CIDR_V6="2001:db8:abcd:003::/64"
+# ELK_NET_B_CIDR_V6="2001:db8:abcd:004::/64"
+# VIR_ROUTER_B_EXT_IP_V6="fill in"           # IPv6 address of the external router
+# HOST_B_IP_V6="fill in"                     # IPv6 address of Host B
 
 # Routing for Host B
 ip route add $5G_NET_B_CIDR_V4 via $VIR_ROUTER_B_EXT_IP_V4
 ip route add $ELK_NET_B_CIDR_V4 via $VIR_ROUTER_B_EXT_IP_V4
-ip -6 route add $5G_NET_B_CIDR_V6 via $VIR_ROUTER_B_EXT_IP_V6
-ip -6 route add $ELK_NET_B_CIDR_V6 via $VIR_ROUTER_B_EXT_IP_V6
+# ip -6 route add $5G_NET_B_CIDR_V6 via $VIR_ROUTER_B_EXT_IP_V6
+# ip -6 route add $ELK_NET_B_CIDR_V6 via $VIR_ROUTER_B_EXT_IP_V6
