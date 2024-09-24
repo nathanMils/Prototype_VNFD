3bfd939a7cc1cd80b571a876f05548d11ce8bd77
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-09-24 14:12:12 +0200

    Update

--- project_files/flavors/add_flavors.sh
+++ project_files/flavors/add_flavors.sh
@@ -4,3 +4,4 @@ openstack flavor create elk_basic --ram 4096 --disk 50 --vcpus 4
 openstack flavor create elk_norm --ram 8192 --disk 100 --vcpus 4
 openstack flavor create nf_basic --ram 2048 --disk 10 --vcpus 2
 openstack flavor create nf_heavy --ram 4096 --disk 20 --vcpus 3
+openstack flavor create ueransim_basic --ram 4096 --disk 20 --vcpus 3
