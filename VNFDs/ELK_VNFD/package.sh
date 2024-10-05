887477f0d073f96a26804456ebe4f1113e3c6793
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-10-05 14:26:15 +0200

    Update

--- VNFDs/ELK_VNFD/package.sh
+++ VNFDs/ELK_VNFD/package.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -e  # Exit on error
+meta_file_path='TOSCA-Metadata/TOSCA.meta'
+cp 'TOSCA-Metadata/TOSCA.template.meta' "$meta_file_path"
 zip -r vnfd_package.zip TOSCA-Metadata/TOSCA.meta Definitions/ BaseHOT/ UserData/
