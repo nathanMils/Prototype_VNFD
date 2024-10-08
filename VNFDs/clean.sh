f74f54bb3366c1198563f58056e7da04c875b69a
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-10-08 14:14:23 +0200

    Auto scripts

---
+++ VNFDs/clean.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+delete_vnf_descriptors() {
+    echo "Deleting all VNF Descriptors..."
+
+    vnfd_ids=$(openstack vnf package list -c Id -f value)
+
+    if [ -n "$vnfd_ids" ]; then
+        echo "$vnfd_ids" | while read -r vnfd_id; do
+            echo "Disabling and Deleting VNFD $vnfd_id..."
+            openstack vnf package disable "$vnfd_id"
+        done
+        echo "All VNF Descriptors deleted."
+    else
+        echo "No VNF Descriptors found."
+    fi
+}
+
+delete_vnf_descriptors
+
+echo "All VNF packages and descriptors have been removed."
