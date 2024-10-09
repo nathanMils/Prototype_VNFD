1adf0b755dd8c5d98b3eddca70d11c06ffa1b942
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-10-09 02:54:14 +0200

    Update Prototype v6 SHA vals

--- project_files/flavors/add_flavors.sh
+++ project_files/flavors/add_flavors.sh
@@ -5,3 +5,5 @@ openstack flavor create elk_norm --ram 8192 --disk 100 --vcpus 4
 openstack flavor create nf_basic --ram 2048 --disk 10 --vcpus 2
 openstack flavor create nf_heavy --ram 4096 --disk 20 --vcpus 3
 openstack flavor create ueransim_basic --ram 4096 --disk 20 --vcpus 3
+openstack flavor create vpp_upf --ram 4096 --disk 10 --vcpus 3
+openstack flavor create nf_light --ram 1024 --disk 5 --vcpus 1
