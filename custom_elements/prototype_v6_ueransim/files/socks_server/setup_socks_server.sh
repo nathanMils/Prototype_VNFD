a79a08cccf68fbf971cb8279f8342eef69d9bbdd
nathanMils <106823768+nathanMils@users.noreply.github.com>
2024-10-10 18:24:20 +0200

    Update UERANSIM element

--- custom_elements/prototype_v6_ueransim/files/socks_server/setup_socks_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-PDU_SESSION_ID=$1
-HOST_PORT=$2
-CONTAINER_NAME=$3
-
-/UERANSIM/build/nr-binder $PDU_SESSION_ID /opt/prototype_master/socks_server/run_server.sh $HOST_PORT $CONTAINER_NAME
---
+++ custom_elements/prototype_v6_ueransim/files/socks_server/start_proxies.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+TEMPLATE_CONFIG=/opt/prototype_master/socks_server/sockd.template.conf
+CONFIG_DIR=/opt/prototype_master/socks_server/configs
+
+matching_interfaces=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^uetunsim[0-9]+$')
+
+for interface in $matching_interfaces; do
+    NUMERIC_PART=$(echo "$interface" | grep -o '[0-9]\+')
+    PORT=$((1080 + NUMERIC_PART))
+    
+    cp "$TEMPLATE_CONFIG" "$CONFIG_DIR/sockd.$interface.conf"
+    
+    sed -i "s|<PORT>|$PORT|g; s|<EXTERNAL_INTERFACE>|$interface|g" "$CONFIG_DIR/sockd.$interface.conf"
+done
+
+echo "Configuration files created successfully."
+
+echo "Pulling Dante SOCKS server image..."
+docker pull vimagick/dante
+
+echo "Starting SOCKS proxies..."
+for interface in $matching_interfaces; do
+    CONTAINER_NAME="socks_proxy_$interface"
+    
+    docker run -d -p --network host -v $CONFIG_DIR/sockd.$interface.conf:/etc/dante/sockd.conf --name "$CONTAINER_NAME" vimagick/dante
+done
+
+for interface in $matching_interfaces; do
+    ip_address=$(ip -o -4 addr show "$interface" | awk '{print $4}')
+    CONTAINER_NAME="socks_proxy_$interface"
+    NUMERIC_PART=$(echo "$interface" | grep -o '[0-9]\+')
+    PORT=$((1080 + NUMERIC_PART))
+    echo "UE TUN Interface: $interface, IP Address: $ip_address, Port: $PORT, SOCKS Proxy Container: $CONTAINER_NAME"
+done
